cd7c2a835551a5bb10269169718fcd94e44e07ee
David Landeros <david.landeros@gameloft.com>
2017-07-03 15:34:46 -0700

    Add files

---
+++ SimpleCrypter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleCrypter
+{
+    class Program
+    {
+
+        static void ShowUsage()
+        {
+            Console.WriteLine("Usage: ");
+            Console.WriteLine("    SimpleCrypter <executable file> <key> <output file>\r\n");
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 3)
+            {
+                ShowUsage();
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("No such file: " + args[1]);
+                return;
+            }
+
+            if (!File.Exists("stub.bin"))
+            {
+                Console.WriteLine("No such file: stub.bin");
+                return;
+            }
+
+            FileStream stub = new FileStream("stub.bin", FileMode.Open);
+            BinaryReader stubReader = new BinaryReader(stub);
+            FileStream target = new FileStream(args[0], FileMode.Open);
+            BinaryReader targetReader = new BinaryReader(target);
+            FileStream output = new FileStream(args[2], FileMode.Create);
+            BinaryWriter outputWriter = new BinaryWriter(output);
+
+            /* Write Stub binary */
+            outputWriter.Write(stubReader.ReadBytes((int)stub.Length));
+            /* Write key */
+            outputWriter.Write(args[1]);
+            /* Write target length (int32) */
+            outputWriter.Write((int)target.Length);
+            /* Encrypt target binary */
+            byte[] targetBinary = targetReader.ReadBytes((int)target.Length);
+            byte[] key = System.Text.Encoding.ASCII.GetBytes(args[1]);
+            byte[] cryptogram = RC4.Encrypt(key, targetBinary);
+            /* Write the cryptogram */
+            outputWriter.Write(cryptogram);
+            outputWriter.Flush();
+            outputWriter.Close();
+            output.Close();
+            target.Close();
+            stub.Close();
+        }
+    }
+}
---
+++ SimpleLauncher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace SimpleLauncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                int selfSize = 8192;
+                string selfFileName = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", "");
+                FileStream self = new FileStream(selfFileName, FileMode.Open, FileAccess.Read);
+                BinaryReader selfReader = new BinaryReader(self);
+                selfReader.ReadBytes(selfSize);
+                string str_key = selfReader.ReadString();
+                byte[] key = System.Text.Encoding.ASCII.GetBytes(str_key);
+                int length = selfReader.ReadInt32();
+                byte[] cryptogram = selfReader.ReadBytes(length);
+                byte[] program = RC4.Decrypt(key, cryptogram);
+                Assembly.Load(program).EntryPoint.Invoke(null, null);
+            }
+            catch (Exception err)
+            {
+                //System.Windows.Forms.MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
