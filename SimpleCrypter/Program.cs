f96a7fdbbbdcfd966e4f5727d8d40fb994833a18
David Landeros <david.landeros@gameloft.com>
2017-07-03 19:21:38 -0700

    Store launcher length at last 4 bytes of the output file

--- SimpleCrypter/Program.cs
+++ SimpleCrypter/Program.cs
@@ -5,33 +5,40 @@ namespace SimpleCrypter
 {
     class Program
     {
-
+        /// <summary>
+        /// It shows the usage of the program
+        /// </summary>
         static void ShowUsage()
         {
             Console.WriteLine("Usage: ");
             Console.WriteLine("    SimpleCrypter <executable file> <key> <output file>\r\n");
         }
 
+        /// <summary>
+        /// Entry point of the program
+        /// </summary>
         static void Main(string[] args)
         {
+            // Checking arguments count
             if(args.Length != 3)
             {
                 ShowUsage();
                 return;
             }
 
+            // Checking whether the required files exist
             if (!File.Exists(args[0]))
             {
                 Console.WriteLine("No such file: " + args[1]);
                 return;
             }
-
             if (!File.Exists("stub.bin"))
             {
                 Console.WriteLine("No such file: stub.bin");
                 return;
             }
 
+            // Opening required files
             FileStream stub = new FileStream("stub.bin", FileMode.Open);
             BinaryReader stubReader = new BinaryReader(stub);
             FileStream target = new FileStream(args[0], FileMode.Open);
@@ -39,19 +46,23 @@ namespace SimpleCrypter
             FileStream output = new FileStream(args[2], FileMode.Create);
             BinaryWriter outputWriter = new BinaryWriter(output);
 
-            /* Write Stub binary */
+            // Create a new file using the following format:
+            // [ stub binary ]
+            // [ encryption key]
+            // [ length of the target binary ]
+            // [ target binary (encrypted) ]
+            // [ length of the stub (Last 4 bytes)]
             outputWriter.Write(stubReader.ReadBytes((int)stub.Length));
-            /* Write key */
             outputWriter.Write(args[1]);
-            /* Write target length (int32) */
             outputWriter.Write((int)target.Length);
-            /* Encrypt target binary */
             byte[] targetBinary = targetReader.ReadBytes((int)target.Length);
             byte[] key = System.Text.Encoding.ASCII.GetBytes(args[1]);
             byte[] cryptogram = RC4.Encrypt(key, targetBinary);
-            /* Write the cryptogram */
             outputWriter.Write(cryptogram);
+            outputWriter.Write((int)stub.Length);
             outputWriter.Flush();
+
+            // clean up everything
             outputWriter.Close();
             output.Close();
             target.Close();
--- SimpleLauncher/Program.cs
+++ SimpleLauncher/Program.cs
@@ -18,22 +18,31 @@ namespace SimpleLauncher
         {
             try
             {
-                int selfSize = 8192;
+                // Opens the executing assembly in read only mode
                 string selfFileName = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", "");
                 FileStream self = new FileStream(selfFileName, FileMode.Open, FileAccess.Read);
                 BinaryReader selfReader = new BinaryReader(self);
-                selfReader.ReadBytes(selfSize);
+
+                // Reads the last 4 bytes containing the length of the stub 
+                selfReader.ReadBytes((int)self.Length - 4);
+                int selfSize = selfReader.ReadInt32();
+
+                // Sets the position at the end of the stub 
+                selfReader.BaseStream.Seek(selfSize, SeekOrigin.Begin);
+
+                // Reads the Key, the Length and the Cryptogram 
                 string str_key = selfReader.ReadString();
                 byte[] key = System.Text.Encoding.ASCII.GetBytes(str_key);
                 int length = selfReader.ReadInt32();
                 byte[] cryptogram = selfReader.ReadBytes(length);
+
+                // Decrypts the program 
                 byte[] program = RC4.Decrypt(key, cryptogram);
+
+                // Loads the program and invokes it's entry point
                 Assembly.Load(program).EntryPoint.Invoke(null, null);
             }
-            catch (Exception err)
-            {
-                //System.Windows.Forms.MessageBox.Show(err.Message);
-            }
+            catch {  }
         }
     }
 }
